63cdfcba32ff04c50973684fa0cdd0d912f1dd74
msjo91 <msjo91@gmail.com>
2017-05-01 23:51:27 +0900

    Create test_was_published_recently_with_recent_question()

--- django_app/polls/tests.py
+++ django_app/polls/tests.py
@@ -16,3 +16,8 @@ class QuestionMethodTests(TestCase):
         time = timezone.now() - datetime.timedelta(days=30)
         old_question = Question(pub_date=time)
         self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
