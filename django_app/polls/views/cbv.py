ed0721765110c6b4453f3dbc4f5390e48900a7d6
msjo91 <msjo91@gmail.com>
2017-05-02 00:02:36 +0900

    Add filter(pub_date__lte)

--- django_app/polls/views/cbv.py
+++ django_app/polls/views/cbv.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.views import generic
 
 from ..models import Question
@@ -8,7 +9,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
