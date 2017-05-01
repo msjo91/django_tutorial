953190c00dac8769025b5c489714a1a715e02cae
msjo91 <msjo91@gmail.com>
2017-05-01 23:33:41 +0900

    Create CBV and reroute urls

--- django_app/polls/urls.py
+++ django_app/polls/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 
-from . import views
+from .views import cbv, fbv
 
 app_name = 'polls'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^$', cbv.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', cbv.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', cbv.ResultsView.as_view(), name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', fbv.vote, name='vote'),
 ]
